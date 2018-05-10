0b2a4bfefd0632c7ca3870076623d734fef89352
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-05-10 16:48:15 +0200

    Prepare for release

--- lib/membrane_element_fake/sink_buffers.ex
+++ lib/membrane_element_fake/sink_buffers.ex
@@ -1,18 +1,24 @@
 defmodule Membrane.Element.Fake.Sink.Buffers do
+  @moduledoc """
+  Fake sink that ignores coming data. It makes demands in buffers.
+  """
+
   use Membrane.Element.Base.Sink
-  def_known_sink_pads %{
-    :sink => {:always, {:pull, demand_in: :buffers}, :any}
-  }
 
+  def_known_sink_pads sink: {:always, {:pull, demand_in: :buffers}, :any}
 
-  @impl
+  @impl true
   def handle_init(_) do
     {:ok, nil}
   end
 
+  @impl true
+  def handle_play(state) do
+    {{:ok, demand: :sink}, state}
+  end
 
-  @impl
-  def handle_write1(:sink, _, _, _) do
-    {{:ok, demand: {:sink, 1}}, nil}
+  @impl true
+  def handle_write1(:sink, _, _, state) do
+    {{:ok, demand: :sink}, state}
   end
 end
