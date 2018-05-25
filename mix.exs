3524b8a5be8e7c6415a1232eb9c27326c789b9e5
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-05-25 16:30:18 +0200

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -9,8 +9,7 @@ defmodule Membrane.Element.Fake.Mixfile do
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Membrane Multimedia Framework (Fake Element)",
-      maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      package: package(),
       name: "Membrane Element: Fake",
       source_url: "https://github.com/membraneframework/membrane-element-fake",
       deps: deps()
@@ -26,6 +25,13 @@ defmodule Membrane.Element.Fake.Mixfile do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"]
+    ]
+  end
 
   defp deps do
     [
