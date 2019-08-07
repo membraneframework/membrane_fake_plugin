e41706e0cfe3c2c9e2856ab01e95e72a2ef31151
DominikWolek <ddww12@gmail.com>
2019-08-07 13:51:58 +0200

    Replace deprecated element

--- lib/membrane_element_fake/sink_bytes.ex
+++ lib/membrane_element_fake/sink_bytes.ex
@@ -3,7 +3,7 @@ defmodule Membrane.Element.Fake.Sink.Bytes do
   Fake sink that ignores coming data. It makes demands in bytes.
   """
 
-  use Membrane.Element.Base.Sink
+  use Membrane.Sink
 
   def_options demand_size: [
                 type: :integer,
