1712232c184ac90f1bd8fb23e13202a2ed2a58c8
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:03 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -58,17 +58,16 @@ defmodule Membrane.Fake.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 1.0"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:membrane_file_plugin, "~> 0.16.0", only: [:dev, :test]}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
